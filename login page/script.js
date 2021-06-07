d21cc210f278da234777cbaf603a5c0ade8ba98d
project-developers <80718041+project-developers@users.noreply.github.com>
2021-06-07 21:50:35 +0000

    Update script.js

--- login page/script.js
+++ login page/script.js
@@ -1,3 +1,20 @@
+function displayNotification() {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      var options = {
+        body: 'Here is a notification body!',
+        icon: 'images/example.png',
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1
+        }
+      };
+      reg.showNotification('Hello world!', options);
+    });
+  }
+}
+
 const button = document.querySelector('.button');
 
 const add = document.querySelector('.add');

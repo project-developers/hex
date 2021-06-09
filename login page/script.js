6fd8f6f3efc3574c00526d5d5c7338459da262ef
project-developers <80718041+project-developers@users.noreply.github.com>
2021-06-09 22:31:14 +0000

    Update script.js

--- login page/script.js
+++ login page/script.js
@@ -1149,7 +1149,8 @@ function addNewPublisher(){
 	
 	congregationData[0].name = prompt("Publisher Name", congregationData[0].name);
 	congregationData[0].fieldServiceGroup = prompt("Field Service Group", congregationData[0].fieldServiceGroup);
-	congregationData.push(congregationData.slice(0,1));
+	var newCard = congregationData.slice(0,1);
+	congregationData.push(newCard[0]);
 	congregationData[0].name = "LASTNAME FirstName";
 	congregationData[0].fieldServiceGroup = "Field Service Group";
 	

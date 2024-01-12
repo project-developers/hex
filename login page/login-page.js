6d5fa5cbdaff413ca13c322a4b0ff554e6bb880e
project-developers <80718041+project-developers@users.noreply.github.com>
2024-01-12 00:51:57 +0000

    modified:   login page/login-page.js

--- login page/login-page.js
+++ login page/login-page.js
@@ -7,7 +7,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username.toLowerCase() === "Reporter".toLowerCase() && password === "reportEntry") {
+    if (username.toLowerCase() === "reporter".toLowerCase() && password === "reportEntry") {
         //alert("You have successfully logged in.");
         location.href = "report.html";
     } else {
